f3194565b2f095e8d72ecf784601644f74e01e7e
Evans-mutuku <evansnyamai98@gmail.com>
2025-11-03 12:23:21 +0300

    ready to test chatbot

--- chatbot.py
+++ chatbot.py
@@ -30,7 +30,7 @@ st.caption("Ask me anything about software Engineering")
 # Sidebar for config
 with st.sidebar:
     st.header("Model Controls/Config")
-    max_new_tokens = st.slider("Max New Tokens", min_value=10, max_value=100, value=50, step=10)
+    max_new_tokens = st.slider("Max New Tokens", min_value=10, max_value=300, value=50, step=10)
     temperature = st.slider("Temperature", min_value=0.1, max_value=1.0, value=0.5, step=0.1)
     
     if st.button("Clear Chat"):
